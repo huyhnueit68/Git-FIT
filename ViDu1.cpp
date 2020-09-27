590cf4bc2818ae0f6e6835ddfc90750b182548b8
huyhnueit68 <huypq_sv@hnue.edu.vn>
2020-09-27 17:27:38 +0700

    add new file cpp

--- ViDu1.cpp
+++ ViDu1.cpp
@@ -4,15 +4,12 @@ int main(int a, char *n[]){
 	cout<<"The world hello Huy" <<endl;
 	cout<<"So luong tham so: "<< a <<endl;
 	
-	cout<<"\n"<<n[0];
-	cout<<"\n"<<n[1];
-	cout<<"\n"<<n[2];
-	cout<<"\n"<<n[3];
 
 	for(int i=0;i<a;i++)
 	{
 		cout<<"\n"<<*(n+i);
 	}
+	//ahihihihi
 	
 	return 0;
 }
